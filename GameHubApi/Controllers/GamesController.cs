24ea4dea670784adfc489602b9135ff1c27c9f78
Anas Chahid <anas.chahid777@gmail.com>
2024-01-15 18:26:15 +0100

    Fix Typo

--- GameHubApi/Controllers/GamesController.cs
+++ GameHubApi/Controllers/GamesController.cs
@@ -2,6 +2,7 @@
 using Domain.Entities;
 using Domain.FetchResponses;
 using GameHubApi.DTOs;
+using Mapster;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,14 +18,14 @@ namespace GameHubApi.Controllers
         public async Task<ActionResult<RawgFetchResponseDTO<GameDTO>>> GetGames(string? genre, string? parentPlatform, string? ordering, string? search, string? page)
         {
             var games = await _rawgApiClient.GetGamesAsync(genre,parentPlatform,ordering,search,page);
-            return Ok(games);
+            return Ok(games.Adapt<RawgFetchResponseDTO<GameDTO>>());
         }
 
         [HttpGet("{slug}")]
-        public async Task<ActionResult<RawgFetchResponseDTO<GameDTO>>> GetGame(string slug)
+        public async Task<ActionResult<GameDTO>> GetGame(string slug)
         {
             var game = await _rawgApiClient.GetGameAsync(slug);
-            return Ok(game);
+            return Ok(game.Adapt<GameDTO>());
         }
     }
 }
