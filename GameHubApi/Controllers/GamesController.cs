bd371c4efc1585cb46b370b2feadca737793a3d3
Anas Chahid <anas.chahid777@gmail.com>
2024-01-15 22:40:40 +0100

    Add logging to the GamesController endpoints calls

--- GameHubApi/Controllers/GamesController.cs
+++ GameHubApi/Controllers/GamesController.cs
@@ -10,13 +10,15 @@ namespace GameHubApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class GamesController(IRawgApiClient rawgApiClient) : ControllerBase
+    public class GamesController(ILogger<GamesController> logger , IRawgApiClient rawgApiClient) : ControllerBase
     {
+        private readonly ILogger<GamesController> _logger = logger;
         private readonly IRawgApiClient _rawgApiClient = rawgApiClient;
 
         [HttpGet]
         public async Task<ActionResult<RawgFetchResponseDTO<GameDTO>>> GetGames(string? genre, string? parentPlatform, string? ordering, string? search, string? page)
         {
+            _logger.LogInformation($"Calling the {nameof(GetGames)} endpoint");
             var games = await _rawgApiClient.GetGamesAsync(genre,parentPlatform,ordering,search,page);
             return Ok(games.Adapt<RawgFetchResponseDTO<GameDTO>>());
         }
@@ -24,6 +26,7 @@ namespace GameHubApi.Controllers
         [HttpGet("{slug}")]
         public async Task<ActionResult<GameDTO>> GetGame(string slug)
         {
+            _logger.LogInformation($"Calling the {nameof(GetGame)} endpoint");
             var game = await _rawgApiClient.GetGameAsync(slug);
             return Ok(game.Adapt<GameDTO>());
         }
@@ -31,6 +34,7 @@ namespace GameHubApi.Controllers
         [HttpGet("{id}/screenshots")]
         public async Task<ActionResult<RawgFetchResponseDTO<GameScreenshotDTO>>> GetGameScreenshots(int id)
         {
+            _logger.LogInformation($"Calling the {nameof(GetGameScreenshots)} endpoint");
             var screenshots = await _rawgApiClient.GetGameScreenshotsAsync(id);
             return Ok(screenshots.Adapt<RawgFetchResponseDTO<GameScreenshotDTO>>());
         }
@@ -38,6 +42,7 @@ namespace GameHubApi.Controllers
         [HttpGet("{id}/movies")]
         public async Task<ActionResult<RawgFetchResponseDTO<GameTrailerDTO>>> GetGameTrailers(int id)
         {
+            _logger.LogInformation($"Calling the {nameof(GetGameTrailers)} endpoint");
             var trailers = await _rawgApiClient.GetGameTrailersAsync(id);
             return Ok(trailers.Adapt<RawgFetchResponseDTO<GameTrailerDTO>>());
         }
