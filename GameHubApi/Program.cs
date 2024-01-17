063950471f205b40c0732ff47c83efbdd3a0b946
Anas Chahid <anas.chahid777@gmail.com>
2024-01-17 16:36:42 +0100

    Update CORS and Cookie type

--- GameHubApi/Program.cs
+++ GameHubApi/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddControllers().AddJsonOptions(opt => opt.JsonSerializerOption
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.ConfigureApplicationCookie(options => { options.Cookie.SameSite = SameSiteMode.None;});
 
 // Register Services
 builder.Services.AddPresentation()
@@ -26,7 +27,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseCors(opt => opt.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+app.UseCors(opt => opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://127.0.0.1:5173").AllowCredentials());
 app.UseOutputCache();
 app.UseHttpsRedirection();
 app.UseExceptionHandler();
